0e007177ba80237c66c59c58c706ccc74d36972b
lixin <736866045@qq.com>
2019-12-09 15:57:31 +0800

    文件上传加打印日志

--- src/main/java/com/love/fallinlove/controller/UploadController.java
+++ src/main/java/com/love/fallinlove/controller/UploadController.java
@@ -1,5 +1,6 @@
 package com.love.fallinlove.controller;
 
+import com.love.utils.LogUtil;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -34,28 +35,32 @@ public class UploadController {
     @RequestMapping(value = "/upload", method = RequestMethod.POST)
     public String upload(@RequestParam(value="file",required=false)MultipartFile file, HttpServletRequest request, HttpServletResponse response, Long userId) {
         String url = "";
-        String fileName=file.getOriginalFilename();//获取文件名加后缀
-        if(fileName!=null&&fileName!=""){
-            String fileF = fileName.substring(fileName.lastIndexOf("."));//文件后缀
-            fileName= System.currentTimeMillis() + "_" + new Random().nextInt(1000) + fileF;//新的文件名
+        try {
+            String fileName=file.getOriginalFilename();//获取文件名加后缀
+            if(fileName!=null&&fileName!=""){
+                String fileF = fileName.substring(fileName.lastIndexOf("."));//文件后缀
+                fileName= System.currentTimeMillis() + "_" + new Random().nextInt(1000) + fileF;//新的文件名
 
 
-            //获取文件夹路径
-            String filePath = STATIC_PATH + SUB_PATH + "/"+ userId;
-            File file1 =new File(filePath);
-            //如果文件夹不存在则创建
-            if(!file1.exists()  && !file1.isDirectory()){
-                file1.mkdirs();
+                //获取文件夹路径
+                String filePath = STATIC_PATH + SUB_PATH + "/"+ userId;
+                File file1 =new File(filePath);
+                //如果文件夹不存在则创建
+                if(!file1.exists()  && !file1.isDirectory()){
+                    file1.mkdirs();
+                }
+                //将图片存入文件夹
+                File targetFile = new File(file1, fileName);
+                try {
+                    //将上传的文件写到服务器上指定的文件。
+                    file.transferTo(targetFile);
+                    url = SUB_PATH + "/"+ userId + "/" + fileName;
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
             }
-            //将图片存入文件夹
-            File targetFile = new File(file1, fileName);
-            try {
-                //将上传的文件写到服务器上指定的文件。
-                file.transferTo(targetFile);
-                url = SUB_PATH + "/"+ userId + "/" + fileName;
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
+        } catch (Exception e) {
+            LogUtil.error(e);
         }
         return url;
     }
